c0756245ba10f2a3a49b3f79758ac7fa1ffa3258
soak43 <sayali.oak.52@gmail.com>
2023-06-12 15:50:33 -0700

    app.use cors origin modified

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
    
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://a5--lustrous-pasca-38b123.netlify.app"],
 }));
 
 app.use(express.json());
